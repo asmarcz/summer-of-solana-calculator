999e943611a828cf7b820b2dc4621843f50bc839
Oliver Tušla <oliver.tusla@seznam.cz>
2023-03-03 12:33:43 +0100

    change app id

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("H5sGxN4aqyuH3StHo1pBXQZNzBgX56jEK7XRakSWVm41");
 
 #[program]
 pub mod calculator {
