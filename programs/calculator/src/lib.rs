685d1c4012c7b7c13733efb35c4bc20f7dab0b64
Oliver Tušla <oliver.tusla@seznam.cz>
2023-03-05 17:04:07 +0100

    basic binary operation implementation

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -1,15 +1,38 @@
 use anchor_lang::prelude::*;
+use anchor_lang::prelude::borsh::{BorshDeserialize, BorshSerialize};
+use anchor_lang::prelude::ProgramError::InvalidArgument;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("H5sGxN4aqyuH3StHo1pBXQZNzBgX56jEK7XRakSWVm41");
 
 #[program]
 pub mod calculator {
+    use BinaryOperator::*;
     use super::*;
 
     pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.greeting = init_message;
+        Ok(())
+    }
+
+    pub fn execute_bin_op(
+        ctx: Context<BinaryOperation>,
+        operator: BinaryOperator,
+        lhs: i64,
+        rhs: i64,
+    ) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = match operator {
+            Division => {
+                if rhs == 0 {
+                    return Err(InvalidArgument);
+                } else { lhs / rhs }
+            }
+            Minus => lhs - rhs,
+            Multiplication => lhs * rhs,
+            Plus => lhs + rhs,
+        };
         Ok(())
     }
 }
@@ -18,6 +41,20 @@ pub mod calculator {
 pub struct Calculator {
     greeting: String,
     result: i64,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub enum BinaryOperator {
+    Division,
+    Minus,
+    Multiplication,
+    Plus,
+}
+
+#[derive(Accounts)]
+pub struct BinaryOperation<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
 }
 
 #[derive(Accounts)]
--- tests/calculator.ts
+++ tests/calculator.ts
@@ -1,16 +1,57 @@
-import * as anchor from "@project-serum/anchor";
-import {Program} from "@project-serum/anchor";
-import {Calculator} from "../target/types/calculator";
+import * as anchor from "@project-serum/anchor"
+import {Program} from "@project-serum/anchor"
+import {Calculator} from "../target/types/calculator"
+
+const {SystemProgram} = anchor.web3
+import {expect} from "chai"
+
+class BinOp {
+	constructor(public name: string, public lhs: number, public rhs: number, public res: number) {
+	}
+}
 
 describe("calculator", () => {
 	// Configure the client to use the local cluster.
-	anchor.setProvider(anchor.AnchorProvider.env());
+	anchor.setProvider(anchor.AnchorProvider.env())
+	const program = anchor.workspace.Calculator as Program<Calculator>
 
-	const program = anchor.workspace.Calculator as Program<Calculator>;
+	const programProvider = program.provider as anchor.AnchorProvider
 
-	it("Is initialized!", async () => {
-		// Add your test here.
-		const tx = await program.methods.initialize().rpc();
-		console.log("Your transaction signature", tx);
-	});
-});
+	const calculatorPair = anchor.web3.Keypair.generate()
+
+	const text = "Simple Calculator"
+
+	it("create calculator instance", async () => {
+		await program.methods.create(text).accounts({
+			calculator: calculatorPair.publicKey,
+			user: programProvider.wallet.publicKey,
+			systemProgram: SystemProgram.programId,
+		}).signers([calculatorPair]).rpc()
+
+		const account = await program.account.calculator.fetch(calculatorPair.publicKey)
+		expect(account.greeting).eq(text)
+	})
+
+	const binOperations = [
+		new BinOp("division", 16, 4, 4),
+		new BinOp("minus", 3, 4, -1),
+		new BinOp("multiplication", 3, 4, 12),
+		new BinOp("plus", 3, 4, 7),
+	]
+	for (let op of binOperations) {
+		it(op.name, async () => {
+			await program.methods
+				.executeBinOp(
+					{[op.name]: {}},
+					new anchor.BN(op.lhs),
+					new anchor.BN(op.rhs),
+				)
+				.accounts({
+					calculator: calculatorPair.publicKey,
+				})
+				.rpc()
+			const account = await program.account.calculator.fetch(calculatorPair.publicKey)
+			expect(account.result).eq(new anchor.BN(op.res))
+		})
+	}
+})
