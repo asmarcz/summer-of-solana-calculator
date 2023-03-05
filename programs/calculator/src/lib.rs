d50da14b9fff75cca5f3cdbd53ab833e32a67b5b
Oliver Tušla <oliver.tusla@seznam.cz>
2023-03-05 12:57:58 +0100

    create calculator from context

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("H5sGxN4aqyuH3StHo1pBXQZNzBgX56jEK7XRakSWVm41");
 
@@ -6,10 +7,24 @@ declare_id!("H5sGxN4aqyuH3StHo1pBXQZNzBgX56jEK7XRakSWVm41");
 pub mod calculator {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
+        Ok({})
     }
 }
 
+#[account]
+pub struct Calculator {
+    greeting: String,
+    result: i64,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 264)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
